78b41a93b65047388ebf4156403aa1dea1f61562
brayanvs001-maker <brayanvs.001@gmail.com>
2026-04-09 15:08:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function comprar(nombre, precio) {
 
-    let numero = "503XXXXXXXX"; // 🔴 CAMBIA ESTO POR TU NUMERO REAL
+    let numero = "50371029678"; // 🔴 CAMBIA ESTO POR TU NUMERO REAL
 
     let mensaje = `Hola, quiero comprar: ${nombre} - $${precio}`;
 
-    let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
+    let url = `https://wa.me/${71029678}?text=${encodeURIComponent(mensaje)}`;
 
     window.open(url, "_blank");
 }
